78ff13f26d9a1c1a71d250c16150c218f978d21a
XAfker <podlinskibartosz@gmail.com>
2023-06-26 16:01:31 +0200

    added basic style

--- seeds/index.js
+++ seeds/index.js
@@ -29,9 +29,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() *20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/45085026',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aut ullam laborum reiciendis saepe, sint, esse doloremque ad voluptas corrupti perferendis beatae blanditiis quis eum sed delectus enim mollitia maxime. Explicabo debitis reprehenderit aperiam quasi eaque id molestias quia et iusto cum ipsa quaerat corrupti sequi nihil architecto earum dignissimos officia, sunt, quas unde ut assumenda impedit sapiente sit! Delectus. Sunt expedita laborum consectetur porro maxime fugiat tenetur deleniti a aliquid molestias quod, dignissimos placeat accusamus dicta, doloribus consequatur earum distinctio voluptatem quisquam? Asperiores ex at accusantium blanditiis nobis saepe.',
+            price
         })
         await camp.save();
     }
